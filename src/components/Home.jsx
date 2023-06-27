fbf0166f603206e349961200ab9bed657ffebbea
Nick Hansen <nhansen@Users-MacBook-Pro.local>
2023-06-26 19:18:25 -0700

    push test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-wrap justify-around max-w-screen-xl mx-auto my-0">
       {collectionTypes.map((type) => (
-        <LevelCard type={type} key={type.url} />
+        <LevelCard type={type} key={type.url} test={"test"} />
       ))}
       <LevelCard type={{ name: "Quizzes", url: "quizzes" }} />
     </div>
